f9432a08c94267999f12b661982961d3d209524e
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-23 21:26:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-// ====== LOAD DATA ======
 let data = JSON.parse(localStorage.getItem("tasks")) || {
   todo: [],
   doing: [],
@@ -12,10 +10,9 @@ function save() {
   render();
 }
 
-// ====== ADD TASK ======
 function addTask() {
   const input = document.getElementById("taskInput");
-  if (!input.value) return;
+  if (!input.value.trim()) return;
 
   data.todo.push({
     id: Date.now(),
@@ -26,59 +23,56 @@ function addTask() {
   save();
 }
 
-// ====== DELETE TASK ======
 function deleteTask(column, id) {
   data[column] = data[column].filter(t => t.id !== id);
   save();
 }
 
-// ====== DRAG & DROP ======
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.dataset.id);
-  ev.dataTransfer.setData("from", ev.target.dataset.column);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-
-  const id = Number(ev.dataTransfer.getData("text"));
-  const from = ev.dataTransfer.getData("from");
-  const to = ev.currentTarget.querySelector(".list").id;
-
-  const task = data[from].find(t => t.id === id);
-  data[from] = data[from].filter(t => t.id !== id);
-  data[to].push(task);
-
-  save();
-}
-
-// ====== RENDER ======
 function render() {
-  ["todo", "doing", "done"].forEach(col => {
-    const container = document.getElementById(col);
-    container.innerHTML = "";
+  ["todo","doing","done"].forEach(col => {
+    const el = document.getElementById(col);
+    el.innerHTML = "";
 
     data[col].forEach(task => {
       const div = document.createElement("div");
       div.className = "task";
       div.draggable = true;
-      div.dataset.id = task.id;
-      div.dataset.column = col;
 
-      div.ondragstart = drag;
+      div.ondragstart = e => {
+        e.dataTransfer.setData("id", task.id);
+        e.dataTransfer.setData("from", col);
+      };
 
       div.innerHTML = `
         ${task.text}
         <span class="delete" onclick="deleteTask('${col}', ${task.id})">✖</span>
       `;
 
-      container.appendChild(div);
+      el.appendChild(div);
     });
   });
 }
+
+function allowDrop(e){ e.preventDefault(); }
+
+function drop(e){
+  e.preventDefault();
+
+  const id = Number(e.dataTransfer.getData("id"));
+  const from = e.dataTransfer.getData("from");
+  const to = e.currentTarget.querySelector(".list").id;
+
+  const task = data[from].find(t => t.id === id);
+
+  data[from] = data[from].filter(t => t.id !== id);
+  data[to].push(task);
+
+  save();
+}
+
+document.querySelectorAll(".column").forEach(col=>{
+  col.ondragover = allowDrop;
+  col.ondrop = drop;
+});
 
 render();
